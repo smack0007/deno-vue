db548d55439050f0ae62a93d8dd84cd6ebef88bd
smack0007 <zachary.snow@gmail.com>
2021-07-14 08:16:00 +0200

    Initial commit.

---
+++ deps.ts
@@ -0,0 +1,1 @@
+export * as Vue from "https://cdn.skypack.dev/vue@3.1.4/dist/vue.esm-browser.prod.js?dts";
---
+++ main.ts
@@ -0,0 +1,19 @@
+import { Vue } from "./deps.ts";
+
+console.info("Starting...");
+
+const App = {
+    template: `
+      <div>
+       <h1>Hello Vue3</h1>
+       <p>{{ message }}</p>
+      </div>
+    `,
+    data() {
+      return {
+        message: 'Oh hi from the component',
+      };
+    },
+  };
+
+Vue.createApp(App).mount('#app');
